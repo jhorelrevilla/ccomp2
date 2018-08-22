232f427054688b8a7b3101b690e3c6a40f787988
jhorel <jhorel.revilla@ucsp.edu.pe>
2018-08-22 05:59:42 -0500

    22/8/18

--- asd/main.cpp
+++ asd/main.cpp
@@ -19,7 +19,6 @@ int conv_100(float &x){
     }
     return cont;
 }
-
 int conv_50(float &x){
     int cont=0;
     while(x>=50){
@@ -68,7 +67,6 @@ int conv_1(float &x){
     }
     return cont;
 }
-
 int conv_5c(float &x){
     int cont=0;
     while(x>=0.50){
@@ -77,7 +75,6 @@ int conv_5c(float &x){
     }
     return cont;
 }
-
 int conv_2c(float &x){
     int cont=0;
     while(x>=0.20){
@@ -86,7 +83,6 @@ int conv_2c(float &x){
     }
     return cont;
 }
-
 int conv_1c(float &x){
     int cont=0;
     while(x>=0.10){
@@ -95,10 +91,14 @@ int conv_1c(float &x){
     }
     return cont;
 }
+///////////////////////////////////////////////////////////
+
+
 
 int main()
 {
-    float x=1538.30;
+    float x;
+    cout<<"ingresa el numero: ";cin>>x;
     cout<<"200: "<<conv_200(x)<<endl;
     cout<<"100: "<<conv_100(x)<<endl;
     cout<<"50: "<<conv_50(x)<<endl;
@@ -110,6 +110,84 @@ int main()
     cout<<"50c: "<<conv_5c(x)<<endl;
     cout<<"20c: "<<conv_2c(x)<<endl;
     cout<<"10c: "<<conv_1c(x)<<endl;
+    ////////////////////////
+    int num,u,d,c,m;
+    u=num%10;
+    numero/=10;
+    d=num%10;
+    numero/=10;
+    c=num%10;
+    numero/=10;
+    m=num%10;
+    numero/=10;
 
+    switch(m){
+        case 1:
+            cout<<"M";break;
+        case 2:
+            cout<<"MM";break;
+        case 3:
+            cout<<"MM";break;
+    }
+    switch(c){
+        case 1:
+            cout<<"C";break;
+        case 2:
+            cout<<"CC";break;
+        case 3:
+            cout<<"CCC";break;
+        case 4:
+            cout<<CD";break;
+        case 5:
+            cout<<"D";break;
+        case 6:
+            cout<<"DC";break;
+        case 7:
+            cout<<"DCC";break;
+        case 8:
+            cout<<"DCCC";break;
+        case 9:
+            cout<<"CM";break;
+    }
+    switch(d){
+        case 1:
+            cout<<"X";break;
+        case 2:
+            cout<<"XX";break;
+        case 3:
+            cout<<"XXX";break;
+        case 4:
+            cout<<"XL";break;
+        case 5:
+            cout<<"L";break;
+        case 6:
+            cout<<"LX";break;
+        case 7:
+            cout<<"LXX";break;
+        case 8:
+            cout<<"LXXX";break;
+        case 9:
+            cout<<"XC";break;
+    }
+    switch(u){
+        case 1:
+            cout<<"I";break;
+        case 2:
+            cout<<"II";break;
+        case 3:
+            cout<<"III";break;
+        case 4:
+            cout<<"IV";break;
+        case 5:
+            cout<<"V";break;
+        case 6:
+            cout<<"VI";break;
+        case 7:
+            cout<<"VII";break;
+        case 8:
+            cout<<"VIII";break;
+        case 9:
+            cout<<"IX";break;
+    }
     return 0;
 }
