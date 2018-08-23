d83e9b7ea5e70054360e2cf81898fe7b7ade6344
jhorel <jhorel.revilla@ucsp.edu.pe>
2018-08-23 17:36:00 -0500

    23.8.18

--- main.cpp
+++ main.cpp
@@ -12,8 +12,7 @@ int modulo(int a, int b){
 int sum_m(int a,int b,int base){
     a+=b;
     int r;
-    cout<<a<<endl;
-    if(a>base ){
+    if(a>=base ){
         r=a-base;
     }
     if(a<0){
@@ -24,8 +23,7 @@ int sum_m(int a,int b,int base){
 int rest_m(int a,int b,int base){
     a-=b;
     int r;
-    cout<<a<<endl;
-    if(a>base ){
+    if(a>=base ){
         r=a-base;
     }
     if(a<0){
@@ -33,25 +31,42 @@ int rest_m(int a,int b,int base){
     }
     return r;
 }
-
+int mult_m(int a,int b,int base){
+    a*=b;
+    int r;
+    if(a>=base ){
+        r=a-base;
+    }
+    if(a<0){
+        r=base+a;
+    }
+    return r;
+}
 int main()
 {
     int a,b,c;
     char op;
     cout<<"ingresa el numero: ";cin>>a;
-    cout<<a<<" "<<"mod"<<endl;
+    cout<<a<<" mod"<<endl;
     cout<<"ingrese la base: ";cin>>c;
-    cout<<a<<" "<<"mod"<<" "<<c<<endl;
+    cout<<a<<" mod "<<c<<endl;
     cout<<"ingrese la operacion: ";cin>>op;
     switch (op){
         case '+':
-        cout<<"ingresa el numero: ";cin>>b;
-        cout<<a<<" "<<"mod"<<" "<<c<<"+"<<b<<" "<<"mod"<<" "<<c<<endl;
-        sum_m(a,b,c);
-        break;
+            cout<<"ingresa el numero: ";cin>>b;
+            cout<<a<<" mod "<<c<<" + "<<b<<" mod "<<c<<endl;
+            cout<<sum_m(a,b,c);
+            break;
         case '-':
-        break;
+            cout<<"ingresa el numero: ";cin>>b;
+            cout<<a<<" mod "<<c<<" - "<<b<<" mod "<<c<<endl;
+            cout<<rest_m(a,b,c);
+            break;
+        case '*':
+            cout<<"ingresa el numero: ";cin>>b;
+            cout<<a<<" mod "<<c<<" * "<<b<<" mod "<<c<<endl;
+            cout<<mult_m(a,b,c);
+            break;
     }
-    //cout<<sum_m(-1,-2,6);
     return 0;
 }
