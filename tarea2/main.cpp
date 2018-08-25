708d27ab2735a300eb70ca387f431d24140c1a0c
jhorel <jhorel.revilla@ucsp.edu.pe>
2018-08-25 10:45:19 -0500

    25.8.18

--- tarea2/main.cpp
+++ tarea2/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 
 using namespace std;
-
+int modulo(int a, int b){
+    int co=a/b;
+    int re=a-co*b;
+    if(re<0){
+        re+=b;
+    }
+    return re;
+}
 int obt_q(int a,int numero){
     int q=1;
     while(a*(q+1)<numero){
@@ -13,13 +20,16 @@ int nani(int a,int b,int numero){//15, ,26
     int q=obt_q(a,numero);
     b=numero-(a*q);
     cout<<numero<<"="<<q<<"*"<<a<<"+"<<b<<endl;
+
+    //int num=a-(b*q);
+    //num=modulo(num,numero);
+    //cout<<"a"<<"-(b"<<"*q): "<<num<<endl;
+
     numero=a;
     a=b;
-
     if(b==1 || b<=0){
         return b;
     }
-
     return nani(a,b,numero);
 }
 bool ver_inver(int a, int b){
@@ -27,21 +37,31 @@ bool ver_inver(int a, int b){
     int cond=99999;
     while(cond>1){
         cond=nani(a,cond,b);
-        cout<< cond;
     }
     if(cond==1){
         return true;
     }
     return false;
 }
-int inver(int a,int b){
-    if(ver_inver(a,b)==true){
 
+int obt_inver(int a,int b,int q,int base){
+    //int num=a-(b*q);
+    //num=modulo(num,base);
+    if(a==0){
+        return 0;
     }
+    if(a==1){
+        return 1;
+    }
+}
+
+
+int inver(int a,int b){
+
 }
 int main()
 {
-    cout<<ver_inver(15,256);
+    cout<<ver_inver(15,26);
 
     return 0;
 }
